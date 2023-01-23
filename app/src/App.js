913247db99ff281a74c3bdea2140b9568e554ecf
malmz90 <70096644+malmz90@users.noreply.github.com>
2023-01-23 16:46:51 +0100

    WIP login/api

--- app/src/App.js
+++ app/src/App.js
@@ -3,40 +3,38 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Navbar from "./components/Navbar";
 const apiKey = process.env.REACT_APP_API_KEY;
+const apiURL = process.env.REACT_APP_API_URL;
 
 function App() {
-  useEffect(() => {
-    const test = async () => {
-      const request = await fetch(
-        `https://cottony-broadleaf-pendulum.glitch.me/translations`,
-        {
-          method: "POST",
-          headers: {
-            "X-API-Key": apiKey,
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            username: "merry Pop",
-            translations: [],
-          }),
-        }
-      );
-      const payload = await request.json();
-      console.log(payload);
-    };
-    // test();
-  }, []);
+    useEffect(() => {
+        const test = async () => {
+            const request = await fetch(`${apiURL}`, {
+                method: "POST",
+                headers: {
+                    "X-API-Key": apiKey,
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    username: "yolo",
+                    translations: [],
+                }),
+            });
+            const payload = await request.json();
+            console.log(payload);
+        };
+        // test();
+    }, []);
 
-  return (
-    <BrowserRouter>
-      <div className="bg-gradient-to-tl from-custom-pink to-custom-blue h-screen">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Login />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <div className="bg-gradient-to-tl from-custom-pink to-custom-blue h-full">
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- app/src/components/Login.js
+++ app/src/components/Login.js
@@ -1,30 +1,68 @@
+import { useForm } from "react-hook-form";
+import { loginUser } from "../api/user";
+
+const usernameConfig = {
+    required: true,
+    minLength: 3,
+};
+
 const Login = () => {
-  return (
-    <div className="grid place-items-center h-screen">
-      <div className="sm:w-4/5 md:w-3/5 lg:w-2/5">
-        <h1 className="text-4xl font-bold text-white mb-2 font-caveat ">
-          Get started by logging in
-        </h1>
-        <div className="bg-gray-200 dark:bg-gray-900 rounded-lg ">
-          <div className="flex flex-col items-center justify-center px-10 py-8 h-32 ">
-            <form
-              className="bg-white w-full flex flex-row rounded-lg  "
-              action="#"
-            >
-              <input
-                className="bg-white text-gray-900 sm:text-sm rounded-lg block w-full p-2.5 outline-none"
-                placeholder="What´s your username?"
-                required=""
-              />
-              <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded m-2">
-                Button
-              </button>
-            </form>
-          </div>
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+
+    const onSubmit = async ({ username }) => {
+        const [error, user] = await loginUser(username);
+        console.log("Error", error);
+        console.log("User", user);
+    };
+
+    const errorMessage = (() => {
+        if (!errors.username) {
+            return null;
+        }
+        if (errors.username.type === "required") {
+            return <span>Username is required</span>;
+        }
+        if (errors.username.type === "minLength") {
+            return <span>Username is to short (min 2 characters)</span>;
+        }
+    })();
+
+    return (
+        <div className="grid place-items-center h-screen">
+            <div className="sm:w-4/5 md:w-3/5 lg:w-2/5">
+                <h1 className="text-4xl font-bold text-white mb-2 font-caveat ">
+                    Get started by logging in
+                </h1>
+                <div className="bg-gray-200 dark:bg-gray-900 rounded-lg ">
+                    <div className="flex flex-col items-center justify-center px-10 py-8 h-32 ">
+                        <form
+                            className="bg-white w-full flex flex-row rounded-lg  "
+                            onSubmit={handleSubmit(onSubmit)}
+                        >
+                            <input
+                                className="bg-white text-gray-900 sm:text-sm rounded-lg block w-full p-2.5 outline-none"
+                                placeholder="What´s your username?"
+                                type="text"
+                                {...register("username", usernameConfig)}
+                            />
+
+                            <button
+                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded m-2"
+                                type="submit"
+                            >
+                                Button
+                            </button>
+                        </form>
+                        {errorMessage}
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Login;
